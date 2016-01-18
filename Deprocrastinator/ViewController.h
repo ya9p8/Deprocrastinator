18a386741bdb38750449f8afeddca7eb9a6eee62
Yemi Ajibola <ya9p8@mst.edu>
2016-01-18 13:27:34 -0600

    Initial Commit

---
+++ Deprocrastinator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Deprocrastinator
+//
+//  Created by Yemi Ajibola on 1/18/16.
+//  Copyright © 2016 Yemi Ajibola. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
